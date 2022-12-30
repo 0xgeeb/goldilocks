391ba541ba83924fbfb4d41afb3e73f2c497810c
0xgeeb <0xgeeb@gmail.com>
2022-12-30 17:59:54 -0500

    added test honey and faucet buttons, still need to figure out bignumber

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,12 +1,15 @@
 import React, { useEffect, useState } from "react"
 import { ethers } from "ethers"
 import logo from "../images/yellow_transparent_logo.png"
+import abi from "../utils/TestHoney.json"
 
 export default function Nav({ currentAccount, setCurrentAccount, avaxChain, setAvaxChain }) {
 
   useEffect(() => {
     checkConnectionandChain()
   }, [])
+
+  const contractAddy = '0x5F4b5D0B353a7531AD1763F0a3691C11Dae8899B'
 
   async function checkConnectionandChain() {
     const accounts = await ethereum.request({ method: 'eth_accounts'})
@@ -25,6 +28,14 @@ export default function Nav({ currentAccount, setCurrentAccount, avaxChain, setA
     setCurrentAccount(accounts[0])
   }
 
+  async function getTestHoney() {
+    const provider = new ethers.providers.Web3Provider(ethereum)
+    const signer = provider.getSigner()
+    const contractObjectSigner = new ethers.Contract(contractAddy, abi.abi, signer)
+    const mintTx = await contractObjectSigner.mint(currentAccount, BigNumber.from(100 * Math.pow(10, 18)));
+    mintTx.wait()
+  }
+
   return (
     <div className="w-[100%] mt-8 flex flex-row items-center justify-between px-24">
       <a href="/"><div className="flex flex-row items-center">
@@ -32,6 +43,8 @@ export default function Nav({ currentAccount, setCurrentAccount, avaxChain, setA
         <h1 className="text-[45px] ml-5 font-acme hover:text-slate-500">Goldilocks v0.1</h1>
       </div></a>
       <div className="flex flex-row">
+        <button className="w-24 py-2 text-[18px] bg-slate-200 hover:bg-slate-500 rounded-xl mr-4 font-acme" id="home-button" onClick={getTestHoney} >$honey</button>
+        <a href="https://faucet.avax.network/" target="_blank"><button className="w-24 py-2 text-[18px] bg-slate-200 hover:bg-slate-500 rounded-xl mr-24 font-acme" id="home-button">faucet</button></a>
         <a href="/amm"><button className="w-24 py-2 text-[18px] bg-slate-200 hover:bg-slate-500 rounded-xl mr-4 font-acme" id="home-button">trade</button></a>
         <a href="/staking"><button className="w-24 py-2 text-[18px] bg-slate-200 hover:bg-slate-500 rounded-xl mr-4 font-acme" id="home-button">stake</button></a>
         <a href="/borrowing"><button className="w-24 py-2 text-[18px] bg-slate-200 hover:bg-slate-500 rounded-xl mr-4 font-acme" id="home-button">borrow</button></a>
