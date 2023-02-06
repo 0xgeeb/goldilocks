c940343e7325eef0ffcc8233532212612e2a8596
0xgeeb <0xgeeb@gmail.com>
2023-02-06 14:08:29 -0500

    corrected borrow tests

--- foundry/test/Borrow.t.sol
+++ foundry/test/Borrow.t.sol
@@ -1,7 +1,7 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.17;
 
-import { Test } from "../lib/forge-std/src/Test.sol";
+import "../lib/forge-std/src/Test.sol";
 import { Locks } from "../src/Locks.sol";
 import { Porridge } from "../src/Porridge.sol";
 import { AMM } from "../src/AMM.sol";
@@ -22,64 +22,59 @@ contract BorrowTest is Test {
     amm = new AMM(address(locks), address(this));
     borrow = new Borrow(address(amm), address(locks), address(this));
     porridge = new Porridge(address(amm), address(locks), address(borrow), address(this));
-    porridge.setLocksAddress(address(locks));
-    borrow.setPorridge(address(porridge));
     locks.setAmmAddress(address(amm));
     locks.setPorridgeAddress(address(porridge));
     locks.setHoneyAddress(address(testhoney));
+    porridge.setLocksAddress(address(locks));
     porridge.setHoneyAddress(address(testhoney));
+    amm.setHoneyAddress(address(testhoney));
+    borrow.setHoneyAddress(address(testhoney));
+    borrow.setPorridge(address(porridge));
+    porridge.approveBorrowForLocks(address(borrow));
+    amm.approveBorrowForHoney(address(borrow));
+    testhoney.mint(address(locks), 1000000e18);
+    locks.transferToAMM(700000e18, 200000e18);
     deal(address(testhoney), address(this), 1000000e18, true);
     deal(address(locks), address(this), 1000000e18, true);
-    deal(address(testhoney), address(locks), 1000000e18, true);
-    locks.setAmmAddress(address(amm));
-    locks.transferToAMM(1600000e18, 400000e18);
+    locks.approve(address(porridge), 1000000e18);
+    testhoney.approve(address(borrow), 1000000e18);
     vm.store(address(amm), bytes32(uint256(5)), bytes32(uint256(1000e18)));
   }
 
   function testBorrow() public {
-    locks.approve(address(porridge), 1000e18);
-    porridge.stake(1000e18);
-    vm.prank(address(porridge));
-    locks.approve(address(borrow), 100e18);
-    vm.prank(address(amm));
-    testhoney.approve(address(borrow), 100e18);
-    uint256 result = borrow.borrow(5e18);
-    assertEq(result, 4850000);
+    porridge.stake(50e18);
+    uint256 floorPrice = amm.floorPrice();
+    uint256 borrowAmount = (50e18 * floorPrice) / 1e18;
+    uint256 result = borrow.borrow(borrowAmount);
+    uint256 fee = (borrowAmount / 100) * 3;
+    assertEq(result, borrowAmount - fee);
   }
 
-function testLimitBorrow() public {
-    locks.approve(address(porridge), 1000000e18);
-    porridge.stake(1000e18);
+function testBorrowLimit() public {
+    porridge.stake(50e18);
+    uint256 floorPrice = amm.floorPrice();
+    uint256 borrowAmount = (50e18 * floorPrice) / 1e18;
     vm.expectRevert(bytes("insufficient borrow limit"));
-    borrow.borrow(9000e18);
+    borrow.borrow(borrowAmount + 1e18);
   }
 
   function testRepay() public {
-    locks.approve(address(porridge), 1000e18);
-    porridge.stake(1000e18);
-    vm.prank(address(porridge));
-    locks.approve(address(borrow), 100000e18);
-    vm.prank(address(amm));
-    testhoney.approve(address(borrow), 100e18);
-    borrow.borrow(8e18);
-    testhoney.approve(address(borrow), 1000e18);
-    locks.approve(address(borrow), 1000e18);
-    borrow.repay(8e18);
-    
+    porridge.stake(50e18);
+    uint256 floorPrice = amm.floorPrice();
+    uint256 borrowAmount = (50e18 * floorPrice) / 1e18;
+    borrow.borrow(borrowAmount);
+    borrow.repay(borrowAmount);
+    assertEq(borrow.getLocked(address(this)), 0);
+    assertEq(borrow.getBorrowed(address(this)), 0);
   }
 
   function testOverRepay() public {
-    locks.approve(address(porridge), 1000e18);
-    porridge.stake(1000e18);
-    vm.prank(address(porridge));
-    locks.approve(address(borrow), 100000e18);
-    vm.prank(address(amm));
-    testhoney.approve(address(borrow), 100e18);
-    borrow.borrow(8e18);
-    testhoney.approve(address(borrow), 100e18);
-    locks.approve(address(borrow), 100e18);
+    porridge.stake(50e18);
+    uint256 floorPrice = amm.floorPrice();
+    uint256 borrowAmount = (50e18 * floorPrice) / 1e18;
+    borrow.borrow(borrowAmount);
     vm.expectRevert(bytes("repaying too much"));
-    borrow.repay(8000002000000000000);
+    borrow.repay(borrowAmount + 1e18);
   }
 
 }
