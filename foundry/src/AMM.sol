1b2304d00ab24b2b6c8befd2ec69a5ec1a86b3d3
0xgeeb <0xgeeb@gmail.com>
2023-03-31 18:40:58 -0400

    starting to use solady

--- foundry/src/AMM.sol
+++ foundry/src/AMM.sol
@@ -1,10 +1,13 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.17;
 
+import { FixedPointMathLib } from "../lib/solady/src/utils/FixedPointMathLib.sol";
 import { IERC20 } from "../lib/openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
 import { ILocks } from "./interfaces/ILocks.sol";
 
 contract AMM {
+
+  using FixedPointMathLib for uint256;
 
   IERC20 honey;
   ILocks ilocks;
@@ -40,7 +43,30 @@ contract AMM {
   }
 
   function floorPrice() external view returns (uint256) {
+    return _floorPrice(fsl, supply);
+  }
+
+  uint256 internal constant WAD = 1e18;
+
+  function newFloorPrice(uint256 _fsl, uint256 _supply) external view returns (uint256 z) {
+    assembly {
+      // Equivalent to `require(y != 0 && (WAD == 0 || x <= type(uint256).max / WAD))`.
+      if iszero(mul(_supply, iszero(mul(WAD, gt(_fsl, div(not(0), WAD)))))) {
+        // Store the function selector of `DivWadFailed()`.
+        mstore(0x00, 0x7c5f487d)
+        // Revert with (offset, size).
+        revert(0x1c, 0x04)
+      }
+      z := div(mul(_fsl, WAD), _supply)
+    }
+  }
+
+  function oldFloorPrice() external view returns (uint256) {
     return (fsl*(1e18)) / supply;
+  }
+
+  function marketPrice() external view returns (uint256) {
+    return _marketPrice(fsl, psl, supply);
   }
 
   function initialize(uint256 _fsl, uint256 _psl) external onlyLocks {
@@ -145,16 +171,22 @@ contract AMM {
     emit Redeem(msg.sender, _amount);
   }
 
-  function _floorPrice(uint256 _fsl, uint256 _supply) public pure returns (uint256) {
-    return (_fsl*(1e18)) / _supply;
+  function _floorPrice(uint256 _fsl, uint256 _supply) internal pure returns (uint256) {
+    return FixedPointMathLib.divWad(_fsl, _supply);
   }
 
-  function _marketPrice(uint256 _fsl, uint256 _psl, uint256 _supply) public pure returns (uint256) {
+  function _marketPrice(uint256 _fsl, uint256 _psl, uint256 _supply) internal pure returns (uint256) {
    uint256 factor1 = _psl * 1e10 / _supply;
    uint256 factor2 = ((_psl + _fsl) * 1e5) / _fsl;
    uint256 exponential = factor2**5;
    uint256 _floorPriceVariable = _fsl * 1e18 /_supply;
    return _floorPriceVariable + ((factor1 * exponential) / (1e17));
+  }
+
+  function soladyMarketPrice() external pure returns (int256) {
+    int256 num1 = 9e18;
+    int256 num2 = 24e18;
+    return FixedPointMathLib.powWad(num1, num2);
   }
  
   function _floorRaise() private {
---
+++ foundry/test/AMMPriceTest.t.sol
@@ -0,0 +1,303 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+import "../lib/forge-std/src/Test.sol";
+import { IERC20 } from "../lib/openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
+import { AMM } from "../src/AMM.sol";
+import { Locks } from "../src/Locks.sol";
+import { TestHoney } from "../src/TestHoney.sol";
+
+contract AMMPriceTest is Test {
+
+  AMM amm;
+  Locks locks;
+  TestHoney honey;
+
+  function setUp() public {
+    locks = new Locks(address(this));
+    amm = new AMM(address(locks), address(this));
+    honey = new TestHoney();
+    deal(address(honey), address(this), 10000000000000000e18, true);
+    deal(address(locks), address(this), 1000000e18, true);
+    deal(address(honey), address(locks), 100000000000000000000000000000000e18, true);
+    locks.setAmmAddress(address(amm));
+    locks.setHoneyAddress(address(honey));
+    amm.setHoneyAddress(address(honey));
+    honey.approve(address(amm), type(uint256).max);
+  }
+
+  function testPurchase1() public {
+    locks.transferToAMM(1600000e18, 400000e18);
+    vm.store(address(amm), bytes32(uint256(5)), bytes32(uint256(1000e18)));
+    uint256 bought;
+    while (bought < 400) {
+      amm.buy(10e18, type(uint256).max);
+      bought += 10;
+    }
+    uint256 solidityMarketPrice = amm.marketPrice();
+    string[] memory inputs = new string[](2);
+    inputs[0] = "python3";
+    inputs[1] = "../testing/amp_tests/purchase_tests/purchase_test1.py";
+    bytes memory result = vm.ffi(inputs);
+    uint256 pythonMarketPrice = abi.decode(result, (uint256));
+    uint256 variance = pythonMarketPrice / 1000;
+    assert(pythonMarketPrice + variance > solidityMarketPrice);
+    assert(pythonMarketPrice - variance < solidityMarketPrice);
+  }
+
+  function testPurchase2() public {
+    locks.transferToAMM(1600000e18, 400000e18);
+    vm.store(address(amm), bytes32(uint256(5)), bytes32(uint256(1000e18)));
+    uint256 bought;
+    while(bought < 55e18) {
+      amm.buy(25e17, type(uint256).max);
+      bought += 25e17;
+    }
+    uint256 solidityMarketPrice = amm.marketPrice();
+    string[] memory inputs = new string[](2);
+    inputs[0] = "python3";
+    inputs[1] = "../testing/amp_tests/purchase_tests/purchase_test2.py";
+    bytes memory result = vm.ffi(inputs);
+    uint256 pythonMarketPrice = abi.decode(result, (uint256));
+    uint256 variance = pythonMarketPrice / 1000;
+    assert(pythonMarketPrice + variance > solidityMarketPrice);
+    assert(pythonMarketPrice - variance < solidityMarketPrice);
+  }
+
+  function testPurchase3() public {
+    locks.transferToAMM(1600000e18, 400000e18);
+    vm.store(address(amm), bytes32(uint256(5)), bytes32(uint256(1000e18)));
+    uint256 bought;
+    while(bought < 8000) {
+      amm.buy(40e18, type(uint256).max);
+      bought += 40;
+    }
+    uint256 solidityMarketPrice = amm.marketPrice();
+    string[] memory inputs = new string[](2);
+    inputs[0] = "python3";
+    inputs[1] = "../testing/amp_tests/purchase_tests/purchase_test3.py";
+    bytes memory result = vm.ffi(inputs);
+    uint256 pythonMarketPrice = abi.decode(result, (uint256));
+    uint256 variance = pythonMarketPrice / 1000;
+    assert(pythonMarketPrice + variance > solidityMarketPrice);
+    assert(pythonMarketPrice - variance < solidityMarketPrice);
+  }
+
+  function testPurchase4() public {
+    locks.transferToAMM(8700000e18, 1900000e18);
+    vm.store(address(amm), bytes32(uint256(5)), bytes32(uint256(2364e18)));
+    uint256 bought;
+    while(bought < 8000) {
+      amm.buy(40e18, type(uint256).max);
+      bought += 40;
+    }
+    uint256 solidityMarketPrice = amm.marketPrice();
+    string[] memory inputs = new string[](2);
+    inputs[0] = "python3";
+    inputs[1] = "../testing/amp_tests/purchase_tests/purchase_test4.py";
+    bytes memory result = vm.ffi(inputs);
+    uint256 pythonMarketPrice = abi.decode(result, (uint256));
+    uint256 variance = pythonMarketPrice / 1000;
+    assert(pythonMarketPrice + variance > solidityMarketPrice);
+    assert(pythonMarketPrice - variance < solidityMarketPrice);
+  }
+  
+  function testMixed1() public {
+    locks.transferToAMM(1600000e18, 400000e18);
+    vm.store(address(amm), bytes32(uint256(5)), bytes32(uint256(1000e18)));
+    amm.buy(38e18, type(uint256).max);
+    amm.sell(45e18, 0);
+    amm.sell(30e18, 0);
+    amm.buy(10e18, type(uint256).max);
+    amm.sell(20e18, 0);
+    amm.sell(10e18, 0);
+    amm.buy(15e18, type(uint256).max);
+    amm.sell(10e18, 0);
+    amm.sell(12e18, 0);
+    amm.buy(45e18, type(uint256).max);
+    amm.buy(8e18, type(uint256).max);
+    amm.sell(8e18, 0);
+    amm.buy(45e18, type(uint256).max);
+    amm.buy(45e18, type(uint256).max);
+    amm.buy(45e18, type(uint256).max);
+    amm.sell(25e18, 0);
+    amm.buy(45e18, type(uint256).max);
+    amm.buy(45e18, type(uint256).max);
+    amm.buy(12e18, type(uint256).max);
+    amm.sell(48e18, 0);
+    uint256 solidityMarketPrice = amm.marketPrice();
+    string[] memory inputs = new string[](2);
+    inputs[0] = "python3";
+    inputs[1] = "../testing/amp_tests/mixed_tests/mixed_test1.py";
+    bytes memory result = vm.ffi(inputs);
+    uint256 pythonMarketPrice = abi.decode(result, (uint256));
+    uint256 variance = pythonMarketPrice / 1000;
+    assert(pythonMarketPrice + variance > solidityMarketPrice);
+    assert(pythonMarketPrice - variance < solidityMarketPrice);    
+  }
+
+  function testMixed2() public {
+    locks.transferToAMM(1600000e18, 400000e18);
+    vm.store(address(amm), bytes32(uint256(5)), bytes32(uint256(1000e18)));
+    amm.buy(65e17, type(uint256).max);
+    amm.redeem(71e17);
+    amm.buy(32e17, type(uint256).max);
+    amm.redeem(29e17);
+    amm.buy(5e18, type(uint256).max);
+    amm.sell(31e17, 0);
+    amm.buy(28e17, type(uint256).max);
+    amm.sell(6e18, 0);
+    amm.redeem(32e17);
+    amm.buy(4e18, type(uint256).max);
+    amm.buy(10e18, type(uint256).max);
+    uint256 solidityMarketPrice = amm.marketPrice();
+    string[] memory inputs = new string[](2);
+    inputs[0] = "python3";
+    inputs[1] = "../testing/amp_tests/mixed_tests/mixed_test2.py";
+    bytes memory result = vm.ffi(inputs);
+    uint256 pythonMarketPrice = abi.decode(result, (uint256));
+    uint256 variance = pythonMarketPrice / 1000;
+    assert(pythonMarketPrice + variance > solidityMarketPrice);
+    assert(pythonMarketPrice - variance < solidityMarketPrice);
+  }
+
+  function testMixed3() public {
+    locks.transferToAMM(973000e18, 360000e18);
+    vm.store(address(amm), bytes32(uint256(5)), bytes32(uint256(6780e18)));
+    amm.buy(6523e17, type(uint256).max);
+    amm.redeem(719e17);
+    amm.buy(32e18, type(uint256).max);
+    amm.redeem(298e18);
+    amm.buy(53e18, type(uint256).max);
+    amm.sell(317e17, 0);
+    amm.buy(286e18, type(uint256).max);
+    amm.sell(651e17, 0);
+    amm.redeem(32e18);
+    amm.buy(47e17, type(uint256).max);
+    amm.buy(123e18, type(uint256).max);
+    uint256 solidityMarketPrice = amm.marketPrice();
+    string[] memory inputs = new string[](2);
+    inputs[0] = "python3";
+    inputs[1] = "../testing/amp_tests/mixed_tests/mixed_test3.py";
+    bytes memory result = vm.ffi(inputs);
+    uint256 pythonMarketPrice = abi.decode(result, (uint256));
+    uint256 variance = pythonMarketPrice / 1000;
+    assert(pythonMarketPrice + variance > solidityMarketPrice);
+    assert(pythonMarketPrice - variance < solidityMarketPrice);
+  }
+
+  function testRedeem1() public {
+    locks.transferToAMM(1600000e18, 400000e18);
+    vm.store(address(amm), bytes32(uint256(5)), bytes32(uint256(1000e18)));
+    uint256 redeemed;
+    while(redeemed < 400) {
+      amm.redeem(10e18);
+      redeemed += 10;
+    }
+    uint256 solidityMarketPrice = amm.marketPrice();
+    string[] memory inputs = new string[](2);
+    inputs[0] = "python3";
+    inputs[1] = "../testing/amp_tests/redeem_tests/redeem_test1.py";
+    bytes memory result = vm.ffi(inputs);
+    uint256 pythonMarketPrice = abi.decode(result, (uint256));
+    uint256 variance = pythonMarketPrice / 1000;
+    assert(pythonMarketPrice + variance > solidityMarketPrice);
+    assert(pythonMarketPrice - variance < solidityMarketPrice);
+  }
+
+  function testRedeem2() public {
+    locks.transferToAMM(1600000e18, 400000e18);
+    vm.store(address(amm), bytes32(uint256(5)), bytes32(uint256(1000e18)));
+    uint256 redeemed;
+    while(redeemed < 50e18) {
+      amm.redeem(25e17);
+      redeemed += 25e17;
+    }
+    uint256 solidityMarketPrice = amm.marketPrice();
+    string[] memory inputs = new string[](2);
+    inputs[0] = "python3";
+    inputs[1] = "../testing/amp_tests/redeem_tests/redeem_test2.py";
+    bytes memory result = vm.ffi(inputs);
+    uint256 pythonMarketPrice = abi.decode(result, (uint256));
+    uint256 variance = pythonMarketPrice / 1000;
+    assert(pythonMarketPrice + variance > solidityMarketPrice);
+    assert(pythonMarketPrice - variance < solidityMarketPrice);
+  }
+
+  function testSale1() public {
+    locks.transferToAMM(1600000e18, 400000e18);
+    vm.store(address(amm), bytes32(uint256(5)), bytes32(uint256(1000e18)));
+    uint256 sold;
+    while(sold < 900e18) {
+      amm.sell(10e18, 0);
+      sold += 10e18;
+    }
+    uint256 solidityMarketPrice = amm.marketPrice();
+    string[] memory inputs = new string[](2);
+    inputs[0] = "python3";
+    inputs[1] = "../testing/amp_tests/sale_tests/sale_test1.py";
+    bytes memory result = vm.ffi(inputs);
+    uint256 pythonMarketPrice = abi.decode(result, (uint256));
+    uint256 variance = pythonMarketPrice / 1000;
+    assert(pythonMarketPrice + variance > solidityMarketPrice);
+    assert(pythonMarketPrice - variance < solidityMarketPrice);
+  }
+
+  function testSale2() public {
+    locks.transferToAMM(1600000e18, 400000e18);
+    vm.store(address(amm), bytes32(uint256(5)), bytes32(uint256(1000e18)));
+    uint256 sold;
+    while(sold < 100e18) {
+      amm.sell(25e17, 0);
+      sold += 25e17;
+    }
+    uint256 solidityMarketPrice = amm.marketPrice();
+    string[] memory inputs = new string[](2);
+    inputs[0] = "python3";
+    inputs[1] = "../testing/amp_tests/sale_tests/sale_test2.py";
+    bytes memory result = vm.ffi(inputs);
+    uint256 pythonMarketPrice = abi.decode(result, (uint256));
+    uint256 variance = pythonMarketPrice / 1000;
+    assert(pythonMarketPrice + variance > solidityMarketPrice);
+    assert(pythonMarketPrice - variance < solidityMarketPrice);
+  }
+
+  function testSale3() public {
+    locks.transferToAMM(800000e18, 200000e18);
+    vm.store(address(amm), bytes32(uint256(5)), bytes32(uint256(1000e18)));
+    uint256 sold;
+    while(sold < 975e18) {
+      amm.sell(25e18, 0);
+      sold += 25e18;
+    }
+    uint256 solidityMarketPrice = amm.marketPrice();
+    string[] memory inputs = new string[](2);
+    inputs[0] = "python3";
+    inputs[1] = "../testing/amp_tests/sale_tests/sale_test3.py";
+    bytes memory result = vm.ffi(inputs);
+    uint256 pythonMarketPrice = abi.decode(result, (uint256));
+    uint256 variance = pythonMarketPrice / 1000;
+    assert(pythonMarketPrice + variance > solidityMarketPrice);
+    assert(pythonMarketPrice - variance < solidityMarketPrice);
+  }
+
+  function testSale4() public {
+    locks.transferToAMM(6300000e18, 3100000e18);
+    vm.store(address(amm), bytes32(uint256(5)), bytes32(uint256(3124e18)));
+    uint256 sold;
+    while(sold < 2000e18) {
+      amm.sell(285e17, 0);
+      sold += 285e17;
+    }
+    uint256 solidityMarketPrice = amm.marketPrice();
+    string[] memory inputs = new string[](2);
+    inputs[0] = "python3";
+    inputs[1] = "../testing/amp_tests/sale_tests/sale_test4.py";
+    bytes memory result = vm.ffi(inputs);
+    uint256 pythonMarketPrice = abi.decode(result, (uint256));
+    uint256 variance = pythonMarketPrice / 1000;
+    assert(pythonMarketPrice + variance > solidityMarketPrice);
+    assert(pythonMarketPrice - variance < solidityMarketPrice);    
+  }
+
+}
