27bc0f7c37ada2b80683e63692d2eec7024b84f2
0xgeeb <0xgeeb@gmail.com>
2023-04-27 23:50:28 -0400

    walletprovider and added constants file

---
+++ pages/utils/index.ts
@@ -0,0 +1,28 @@
+import borrowABI from "./../abi/Borrow.json"
+import locksABI from "./../abi/Locks.json"
+import porridgeABI from "./../abi/Porridge.json"
+import testhoneyABI from "./../abi/TestHoney.json"
+import ammABI from "./../abi/AMM.json"
+
+export const contracts = {
+  amm: {
+    address: '0x1b5F6509B8b4Dd5c9637C8fa6a120579bE33666F',
+    abi: ammABI.abi
+  },
+  locks: {
+    address: '0x461B8AdEDe13Aa786b3f14b05496B93c5148Ad51',
+    abi: locksABI.abi
+  } ,
+  porridge: {
+    address: '0x69B228b9247dF2c1F194f92fC19A340A9F2803f7',
+    abi: porridgeABI.abi
+  } ,
+  honey: {
+    address: '0x29b9439E09d1D581892686D9e00E3481DCDD5f78',
+    abi: testhoneyABI.abi
+  } ,
+  borrow: {
+    address: '0x1b408d277D9f168A8893b1728d3B6cb75929a67d',
+    abi: borrowABI.abi
+  } 
+}
--- providers/WalletProvider.tsx
+++ providers/WalletProvider.tsx
@@ -1,8 +1,81 @@
-import { PropsWithChildren } from "react";
+import { createContext, PropsWithChildren, useContext, useState } from "react";
+import { BigNumber, Signer, ethers } from "ethers"
+import { useAccount, useSigner, useNetwork, useConnect } from "wagmi"
+import { contracts } from "../pages/utils"
+
+const INITIAL_STATE = {
+  balance: {
+    locks: 0,
+    prg: 0,
+    honey: 0
+  },
+  // wallet: undefined,
+  isConnected: false,
+  signer: null,
+  network: null,
+  // getBalance: async (string, any) => {},
+  // updateBalance: async () => {},
+  // ensureAllowance: async () => {}
+}
+
+const WalletContext = createContext(INITIAL_STATE)
 
 export const WalletProvider = (props: PropsWithChildren<{}>) => {
 
   const { children } = props
+
+  const { data: signer, isLoading: signerLoading } = useSigner()
+  const { chain } = useNetwork()
+  const { address, isConnecting: accountLoading } = useAccount()
+  const { isLoading: connectLoading } = useConnect()
+
+  const [balanceState, setBalanceState] = useState(INITIAL_STATE.balance)
+
+  const walletAddress = address;
+  const isConnected = !!walletAddress && !!signer;
+
+  const getBalance = async (address: string, signer: any) => {
+    let response = {
+      locks: 0,
+      prg: 0,
+      honey: 0
+    }
+
+    const locksContract = new ethers.Contract(
+      contracts.locks.address,
+      contracts.locks.abi,
+      signer
+    )
+    const locksTx = await locksContract.balanceOf(address)
+    console.log(locksTx)
+
+  }
+
+
+
+
+
+
+
+
+
+
+
   console.log('walletprovider')
-  return <>{ children }</>
+  return (
+    <WalletContext.Provider
+      value={{
+        balance: balanceState,
+        // wallet: walletAddress || null,
+        isConnected: isConnected,
+        signer: null,
+        network: null,
+        // getBalance: getBalance,
+        // updateBalance,
+        // ensureAllowance
+      }}
+    >
+      { children }
+    </WalletContext.Provider>
+  )
 }
