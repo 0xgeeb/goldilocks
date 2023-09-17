e112aa02446be0e4b9b27dba502ab56932233189
0xgeeb <0xgeeb@gmail.com>
2023-09-16 22:18:09 -0400

    added selected beras

--- frontend/components/goldilend/loanTab/index.tsx
+++ frontend/components/goldilend/loanTab/index.tsx
@@ -6,6 +6,7 @@ import { useGoldilend } from "../../../providers"
 export const LoanTab = () => {
 
   const [infoLoading, setInfoLoading] = useState<boolean>(true)
+  const [selectedBeras, setSelectedBeras] = useState<string[]>([])
 
   const {
     checkBeraBalance,
@@ -23,20 +24,36 @@ export const LoanTab = () => {
     return <span className="loader-small mx-auto"></span>
   }
 
-  return (
-    <div className="w-[100%] h-[100%] flex flex-row">
-      <div className="h-[100%] w-[70%] bg-green-400">
+  const handleBeraClick = (src: string) => {
+    if(selectedBeras.includes(src)) {
+      setSelectedBeras(prev => prev.filter(bera => bera !== src))
+    }
+    else {
+      setSelectedBeras(prev => [...prev, src])
+    }
+  }
 
+  return (
+    <div className="w-[100%] h-[100%] flex flex-row justify-between">
+      <div className="h-[97.5%] mt-[2.5%] w-[67.5%] flex flex-col px-6 bg-white border-2 border-black rounded-xl">
+        <h1 className="font-acme py-4 text-[20px] 2xl:text-[26px]">create loan</h1>
+        <h2 className="font-acme text-[18px] 2xl:text-[24px]">loan amount</h2>
       </div>
-      <div className="h-[100%] w-[30%] flex flex-col bg-blue-400 px-2">
-        <h1 className="font-acme mx-auto underline py-8 text-[18px] 2xl:text-[24px]">your beras</h1>
+      <div className="h-[97.5%] mt-[2.5%] w-[30%] flex flex-col border-2 border-black rounded-xl bg-white px-2">
+        <h1 className="font-acme mx-auto underline py-4 text-[18px] 2xl:text-[24px]">your beras</h1>
         { 
           infoLoading ? loadingElement() : 
-          <div className="flex flex-wrap overflow-y-auto h-[90%] border-2 border-black" id="hide-scrollbar">
+          <div className="flex flex-wrap overflow-y-auto h-[90%]" id="hide-scrollbar">
             {
               beraArray.map((src, index) => (
                 <div key={index} className="w-[50%] py-2">
-                  <img className="ml-[5%] w-[90%] rounded-xl" src={src} alt="bera" id="home-button" />
+                  <img 
+                    className={`ml-[5%] w-[90%] rounded-xl hover:scale-110 hover:cursor-pointer ${selectedBeras.includes(index.toString()) ? "border-4 border-black" : "opacity-75"}`}
+                    onClick={() => handleBeraClick(index.toString())}
+                    src={src}
+                    alt="bera"
+                    id="home-button"
+                  />
                 </div>
               ))
             }
