b1e34ae3ae8df79b16486df47dd1c8171df24678
0xgeeb <0xgeeb@gmail.com>
2023-10-31 23:35:34 -0400

    rewrote mixed_test1

--- foundry/price_tests/mixed_tests/mixed_test1.py
+++ foundry/price_tests/mixed_tests/mixed_test1.py
@@ -1,143 +1,55 @@
-import random
+import sys
+sys.path.append('price_tests')
+from market_functions import buy, sell, floor_raise
 from eth_abi import encode
-#assuming 2 million dollar presale and 1000 initial supply
+
 fsl = 1400000
-supply = 5000
 psl = 400000
+supply = 5000
 floor_price = fsl/supply
 market_price = floor_price + ((psl/supply)*((psl+fsl)/fsl)**5)
-#target ratio
 target = 0.36
-#invested tracks the net amount of capital that has been deposited into the protocol
-#it subtracts the amount withdrawn on sales
 invested = 0
-#prints initial market price
-# print(market_price)
 
-def redeem(_amount):
-  global supply
-  global fsl
-  global psl
-  global floor_price
-  global market_price
-  supply -=_amount
-  fsl -= floor_price*_amount
-  floor_price = fsl/supply
-  market_price = floor_price + ((psl/supply)*((psl+fsl)/fsl)**5)
-  return market_price
+transactions = [
+  (buy, 38),
+  (floor_raise, None),
+  (sell, 45),
+  (sell, 30),
+  (buy, 10),
+  (floor_raise, None),
+  (sell, 20),
+  (sell, 10),
+  (buy, 15),
+  (floor_raise, None),
+  (sell, 10),
+  (sell, 12),
+  (buy, 45),
+  (floor_raise, None),
+  (buy, 8),
+  (floor_raise, None),
+  (sell, 8),
+  (buy, 45),
+  (floor_raise, None),
+  (buy, 45),
+  (floor_raise, None),
+  (buy, 45),
+  (floor_raise, None),
+  (sell, 25),
+  (buy, 45),
+  (floor_raise, None),
+  (buy, 45),
+  (floor_raise, None),
+  (buy, 12),
+  (floor_raise, None),
+  (sell, 48)
+]
 
-def sell(amount):
-  global invested
-  global supply
-  global fsl
-  global psl
-  global floor_price
-  global market_price
-  sale_price = 0
-  while amount >= 1:
-    amount -= 1
-    supply -= 1
-    sale_price += market_price
-    fsl -= floor_price
-    psl -= (market_price - floor_price)
-    floor_price = fsl/supply
-    market_price = floor_price + ((psl/supply)*((psl+fsl)/fsl)**5)
-  supply -= amount
-  sale_price += market_price*amount
-  fsl -= floor_price*amount
-  psl -= (market_price - floor_price)*amount
-  tax = sale_price*0.053
-  fsl+= tax/2
-  psl+= tax/2
-  floor_price = fsl/max(supply, 1)
-  market_price = floor_price + ((psl/supply)*((psl+fsl)/fsl)**5)
-  invested -= (sale_price - tax)    
-  return market_price
-
-def buy(amount):
-  global invested
-  global supply
-  global fsl
-  global psl
-  global floor_price
-  global market_price
-  global target
-  purchase_price = 0
-  for i in range(0, amount):
-    supply += 1
-    purchase_price += market_price
-    # if psl/fsl >= 0.36: all deposited capital goes to fsl
-    if psl/fsl >= 0.5:
-      fsl += market_price
-      floor_price = fsl/supply
-    else:
-      fsl += floor_price
-      psl += (market_price - floor_price)
-      ratio = psl/fsl
-    market_price = floor_price + ((psl/max(supply, 1))*((psl+fsl)/max(fsl, 1))**5)  
-    invested += market_price
-    #print("Price:", market_price, "Floor price:", floor_price)
-  tax = purchase_price*0.003
-  fsl += tax
-  invested += tax
-  floor_price = fsl/supply
-  market_price = floor_price + ((psl/max(supply, 1))*((psl+fsl)/max(fsl, 1))**5)
-  #print("Price:", market_price, "Floor price:", floor_price)
-  return market_price
-
-def floor_raise():
-  global invested
-  global supply
-  global fsl
-  global psl
-  global floor_price
-  global market_price
-  global target
-  ratio = psl/fsl
-  if ratio >= target:
-    multiplier = (0.01*ratio)/0.32                   
-    transfer = psl*multiplier
-    psl -= transfer
-    fsl += transfer
-    floor_price = fsl/supply
-    target = target*1.02
-    floor_price = fsl/supply
-    market_price = floor_price + ((psl/max(supply, 1))*((psl+fsl)/max(fsl, 1))**5)  
-    #print("Floor raise! Ratio:", psl/fsl)
-  return 'raise'
-
-#just a random buy/sell pattern to compare against what happens when you trade through the contracts
-buy(38)
-floor_raise()
-sell(45)
-sell(30)
-buy(10)
-floor_raise()
-sell(20)
-sell(10)
-buy(15)
-floor_raise()
-sell(10)
-sell(12)
-buy(45)
-floor_raise()
-buy(8)
-floor_raise()
-sell(8)
-buy(45)
-floor_raise()
-buy(45)
-floor_raise()
-buy(45)
-floor_raise()
-sell(25)
-buy(45)
-floor_raise()
-buy(45)
-floor_raise()
-buy(12)
-floor_raise()
-sell(48)
+for transaction, amount in transactions:
+  if transaction == floor_raise:
+    target, invested, fsl, psl, supply, floor_price, market_price = transaction(target, invested, fsl, psl, supply, floor_price, market_price)
+  else:
+    invested, fsl, psl, supply, floor_price, market_price = transaction(amount, invested, fsl, psl, supply, floor_price, market_price)
 
 market_price *= (10 ** 18)
 enc = encode(['uint256'], [int(market_price)])
