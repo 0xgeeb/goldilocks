909a64f3b3bc06986447248cef35c5ce9c7364f2
0xgeeb <0xgeeb@gmail.com>
2023-11-01 20:07:27 -0400

    updated sell tests

--- foundry/price_tests/sale_tests/sale_test3.py
+++ foundry/price_tests/sale_tests/sale_test3.py
@@ -1,118 +1,18 @@
-import random
+import sys
+sys.path.append('price_tests')
+from market_functions import sell
 from eth_abi import encode
-#assuming 1 million dollar presale and 1000 initial supply
+
 fsl = 800000
-supply = 1000
 psl = 200000
+supply = 1000
 floor_price = fsl/supply
 market_price = floor_price + ((psl/supply)*((psl+fsl)/fsl)**5)
-#target ratio
 target = 0.36
-#invested tracks the net amount of capital that has been deposited into the protocol
-#it subtracts the amount withdrawn on sales
-invested = 0
-#prints initial market price
-# print(market_price)
 
-def redeem(_amount):
-  global supply
-  global fsl
-  global psl
-  global floor_price
-  global market_price
-  supply -=_amount
-  fsl -= floor_price*_amount
-  floor_price = fsl/supply
-  market_price = floor_price + ((psl/supply)*((psl+fsl)/fsl)**5)
-  return market_price
-
-def sell(amount):
-  global invested
-  global supply
-  global fsl
-  global psl
-  global floor_price
-  global market_price
-  sale_price = 0
-  while amount >= 1:
-    amount -= 1
-    supply -= 1
-    sale_price += market_price
-    fsl -= floor_price
-    psl -= (market_price - floor_price)
-    floor_price = fsl/supply
-    market_price = floor_price + ((psl/supply)*((psl+fsl)/fsl)**5)
-  supply -= amount
-  sale_price += market_price*amount
-  fsl -= floor_price*amount
-  psl -= (market_price - floor_price)*amount
-  tax = sale_price*0.053
-  fsl+= tax/2
-  psl+= tax/2
-  floor_price = fsl/max(supply, 1)
-  market_price = floor_price + ((psl/supply)*((psl+fsl)/fsl)**5)
-  invested -= (sale_price - tax)    
-  return market_price
-  
-def buy(amount):
-  global invested
-  global supply
-  global fsl
-  global psl
-  global floor_price
-  global market_price
-  global target
-  purchase_price = 0
-  for i in range(0, amount):
-    supply += 1
-    purchase_price += market_price
-    # if psl/fsl >= 0.36: all deposited capital goes to fsl
-    if psl/fsl >= 0.5:
-      fsl += market_price
-      floor_price = fsl/supply
-    else:
-      fsl += floor_price
-      psl += (market_price - floor_price)
-      ratio = psl/fsl
-    market_price = floor_price + ((psl/max(supply, 1))*((psl+fsl)/max(fsl, 1))**5)  
-    invested += market_price
-    #print("Price:", market_price, "Floor price:", floor_price)
-  tax = purchase_price*0.003
-  fsl += tax
-  invested += tax
-  floor_price = fsl/supply
-  market_price = floor_price + ((psl/max(supply, 1))*((psl+fsl)/max(fsl, 1))**5)
-  #print("Price:", market_price, "Floor price:", floor_price)
-  return market_price
-
-def floor_raise():
-  global invested
-  global supply
-  global fsl
-  global psl
-  global floor_price
-  global market_price
-  global target
-  ratio = psl/fsl
-  if ratio >= target:
-    multiplier = (0.01*ratio)/0.32                   
-    transfer = psl*multiplier
-    psl -= transfer
-    fsl += transfer
-    floor_price = fsl/supply
-    target = target*1.02
-    floor_price = fsl/supply
-    market_price = floor_price + ((psl/max(supply, 1))*((psl+fsl)/max(fsl, 1))**5)  
-    #print("Floor raise! Ratio:", psl/fsl)
-  return 'raise'
-
-
-#tracks what happens when 800k dollars worth of sells occur continuously with no buys
-#this is what would happen if all presalers except for the team dumped at once
 sold = 0
 while(sold < 975):
-  sell(25)
-  # print("Price:", market_price, "Floor price:", floor_price)
+  fsl, psl, supply, floor_price, market_price = sell(25, fsl, psl, supply, floor_price, market_price)
   sold += 25
 
 market_price *= (10 ** 18)
--- foundry/price_tests/sale_tests/sale_test4.py
+++ foundry/price_tests/sale_tests/sale_test4.py
@@ -1,117 +1,19 @@
-import random
+import sys
+sys.path.append('price_tests')
+from market_functions import sell
 from eth_abi import encode
-#varying initial fsl, psl, supply
+
 fsl = 6300000
-supply = 3124
 psl = 3100000
+supply = 3124
 floor_price = fsl/supply
 market_price = floor_price + ((psl/supply)*((psl+fsl)/fsl)**5)
-#target ratio
 target = 0.36
-#invested tracks the net amount of capital that has been deposited into the protocol
-#it subtracts the amount withdrawn on sales
-invested = 0
-#prints initial market price
-# print(market_price)
 
-def redeem(_amount):
-  global supply
-  global fsl
-  global psl
-  global floor_price
-  global market_price
-  supply -=_amount
-  fsl -= floor_price*_amount
-  floor_price = fsl/supply
-  market_price = floor_price + ((psl/supply)*((psl+fsl)/fsl)**5)
-  return market_price
-
-def sell(amount):
-  global invested
-  global supply
-  global fsl
-  global psl
-  global floor_price
-  global market_price
-  sale_price = 0
-  while amount >= 1:
-    amount -= 1
-    supply -= 1
-    sale_price += market_price
-    fsl -= floor_price
-    psl -= (market_price - floor_price)
-    floor_price = fsl/supply
-    market_price = floor_price + ((psl/supply)*((psl+fsl)/fsl)**5)
-  supply -= amount
-  sale_price += market_price*amount
-  fsl -= floor_price*amount
-  psl -= (market_price - floor_price)*amount
-  tax = sale_price*0.053
-  fsl+= tax/2
-  psl+= tax/2
-  floor_price = fsl/max(supply, 1)
-  market_price = floor_price + ((psl/supply)*((psl+fsl)/fsl)**5)
-  invested -= (sale_price - tax)    
-  return market_price
-  
-def buy(amount):
-  global invested
-  global supply
-  global fsl
-  global psl
-  global floor_price
-  global market_price
-  global target
-  purchase_price = 0
-  for i in range(0, amount):
-    supply += 1
-    purchase_price += market_price
-    # if psl/fsl >= 0.36: all deposited capital goes to fsl
-    if psl/fsl >= 0.5:
-      fsl += market_price
-      floor_price = fsl/supply
-    else:
-      fsl += floor_price
-      psl += (market_price - floor_price)
-      ratio = psl/fsl
-    market_price = floor_price + ((psl/max(supply, 1))*((psl+fsl)/max(fsl, 1))**5)  
-    invested += market_price
-    #print("Price:", market_price, "Floor price:", floor_price)
-  tax = purchase_price*0.003
-  fsl += tax
-  invested += tax
-  floor_price = fsl/supply
-  market_price = floor_price + ((psl/max(supply, 1))*((psl+fsl)/max(fsl, 1))**5)
-  #print("Price:", market_price, "Floor price:", floor_price)
-  return market_price
-
-def floor_raise():
-  global invested
-  global supply
-  global fsl
-  global psl
-  global floor_price
-  global market_price
-  global target
-  ratio = psl/fsl
-  if ratio >= target:
-    multiplier = (0.01*ratio)/0.32                   
-    transfer = psl*multiplier
-    psl -= transfer
-    fsl += transfer
-    floor_price = fsl/supply
-    target = target*1.02
-    floor_price = fsl/supply
-    market_price = floor_price + ((psl/max(supply, 1))*((psl+fsl)/max(fsl, 1))**5)  
-    #print("Floor raise! Ratio:", psl/fsl)
-  return 'raise'
-
-#2000 continuous sells with no buys
 sold = 0
 while (sold < 2000):
-  sell(28.5)
+  fsl, psl, supply, floor_price, market_price = sell(28.5, fsl, psl, supply, floor_price, market_price)
   sold += 28.5
-  # print("Price:", market_price, "Floor price:", floor_price)
 
 market_price *= (10 ** 18)
 enc = encode(['uint256'], [int(market_price)])
