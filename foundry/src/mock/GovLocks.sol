5bc799169f68ea84d5ffa36a1955185dc79d1ccd
0xgeeb <0xgeeb@gmail.com>
2023-11-06 21:33:53 -0500

    adding govlocks

--- foundry/src/mock/GovLocks.sol
+++ foundry/src/mock/GovLocks.sol
@@ -16,28 +16,28 @@ pragma solidity ^0.8.19;
 // ========================================= GovLocks ===========================================
 // ==============================================================================================
 
-// import { ERC20 } from "../../lib/openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
-// import { IERC20 } from "../../lib/openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
-// import { ERC20Permit } from "../../lib/openzeppelin-contracts/contracts/token/ERC20/extensions/ERC20Permit.sol";
-// import { ERC20Votes } from "../../lib/openzeppelin-contracts/contracts/token/ERC20/extensions/ERC20Votes.sol";
-// import { ERC20Wrapper } from "../../lib/openzeppelin-contracts/contracts/token/ERC20/extensions/ERC20Wrapper.sol";
-// import { Nonces } from "../../lib/openzeppelin-contracts/contracts/utils/Nonces.sol";
+import { ERC20 } from "../../lib/openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
+import { IERC20 } from "../../lib/openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
+import { ERC20Permit } from "../../lib/openzeppelin-contracts/contracts/token/ERC20/extensions/ERC20Permit.sol";
+import { ERC20Votes } from "../../lib/openzeppelin-contracts/contracts/token/ERC20/extensions/ERC20Votes.sol";
+import { ERC20Wrapper } from "../../lib/openzeppelin-contracts/contracts/token/ERC20/extensions/ERC20Wrapper.sol";
+import { Nonces } from "../../lib/openzeppelin-contracts/contracts/utils/Nonces.sol";
 
-contract GovLocks {
-// contract GovLocks is ERC20, ERC20Permit, ERC20Votes, ERC20Wrapper {
-  // constructor(
-  //   IERC20 govlocks
-  // ) ERC20("GovLocks", "gLocks") ERC20Permit("GovLocks") ERC20Wrapper(govlocks) {}
 
-  // function decimals() public view override(ERC20, ERC20Wrapper) returns (uint8) {
-  //   return super.decimals();
-  // }
+contract GovLocks is ERC20, ERC20Permit, ERC20Votes, ERC20Wrapper {
+  constructor(
+    IERC20 govlocks
+  ) ERC20("GovLocks", "gLocks") ERC20Permit("GovLocks") ERC20Wrapper(govlocks) {}
 
-  // function _update(address from, address to, uint256 amount) internal override(ERC20, ERC20Votes) {
-  //   super._update(from, to, amount);
-  // }
+  function decimals() public view override(ERC20, ERC20Wrapper) returns (uint8) {
+    return super.decimals();
+  }
 
-  // function nonces(address owner) public view virtual override(ERC20Permit, Nonces) returns (uint256) {
-  //   return super.nonces(owner);
-  // }
+  function _update(address from, address to, uint256 amount) internal override(ERC20, ERC20Votes) {
+    super._update(from, to, amount);
+  }
+
+  function nonces(address owner) public view virtual override(ERC20Permit, Nonces) returns (uint256) {
+    return super.nonces(owner);
+  }
 }
