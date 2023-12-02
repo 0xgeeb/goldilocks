f7ad9e93470dff39f6a67e40377358f94453e1ad
0xgeeb <0xgeeb@gmail.com>
2023-12-02 14:22:17 -0500

    finished govLOCKS?

--- foundry/src/governance/govLOCKS.sol
+++ foundry/src/governance/govLOCKS.sol
@@ -29,6 +29,17 @@ contract govLOCKS is ERC20 {
 
 
   /*´:°•.°+.*•´.*:˚.°*.˚•´.°:°•.°•.*•´.*:˚.°*.˚•´.°:°•.°+.*•´.*:*/
+  /*                           STRUCT                           */
+  /*.•°:°.´+˚.*°.˚:*.´•*.+°.•°:´*.´•*.•°.•°:°.´:•˚°.*°.˚:*.´+°.•*/
+
+
+  struct Checkpoint {
+    uint256 fromBlock;
+    uint256 votes;
+  }
+
+
+  /*´:°•.°+.*•´.*:˚.°*.˚•´.°:°•.°•.*•´.*:˚.°*.˚•´.°:°•.°+.*•´.*:*/
   /*                      STATE VARIABLES                       */
   /*.•°:°.´+˚.*°.˚:*.´•*.+°.•°:´*.´•*.•°.•°:°.´:•˚°.*°.˚:*.´+°.•*/
 
@@ -37,6 +48,8 @@ contract govLOCKS is ERC20 {
   address public goldigov;
 
   mapping(address => address) public delegates;
+  mapping(address => uint256) public numCheckpoints;
+  mapping(address => mapping(uint256 => Checkpoint)) public checkpoints;
 
 
   /*´:°•.°+.*•´.*:˚.°*.˚•´.°:°•.°•.*•´.*:˚.°*.˚•´.°:°•.°+.*•´.*:*/
@@ -71,7 +84,7 @@ contract govLOCKS is ERC20 {
   /*.•°:°.´+˚.*°.˚:*.´•*.+°.•°:´*.´•*.•°.•°:°.´:•˚°.*°.˚:*.´+°.•*/
 
 
-
+  error NoSuchBlock();
 
 
   /*´:°•.°+.*•´.*:˚.°*.˚•´.°:°•.°•.*•´.*:˚.°*.˚•´.°:°•.°+.*•´.*:*/
@@ -80,7 +93,50 @@ contract govLOCKS is ERC20 {
 
 
   event DelegateChanged(address indexed delegator, address indexed fromDelegate, address indexed toDelegate);
+  event DelegateVotesChanged(address indexed delegate, uint256 previousBalance, uint256 newBalance);
 
+
+  /*´:°•.°+.*•´.*:˚.°*.˚•´.°:°•.°•.*•´.*:˚.°*.˚•´.°:°•.°+.*•´.*:*/
+  /*                       VIEW FUNCTIONS                       */
+  /*.•°:°.´+˚.*°.˚:*.´•*.+°.•°:´*.´•*.•°.•°:°.´:•˚°.*°.˚:*.´+°.•*/
+
+
+  /// @notice Returns current votes balance for user
+  /// @param user Address to return votes balance
+  function getCurrentVotes(address user) external view returns (uint256) {
+    uint256 nCheckpoints = numCheckpoints[user];
+    return nCheckpoints > 0 ? checkpoints[user][nCheckpoints - 1].votes : 0;
+  }
+
+  function getPriorVotes(address account, uint256 blockNumber) external view returns (uint256) {
+    if(blockNumber >= block.number) revert NoSuchBlock();
+    uint256 nCheckpoints = numCheckpoints[account];
+    if (nCheckpoints == 0) {
+      return 0;
+    }
+    // First check most recent balance
+    if (checkpoints[account][nCheckpoints - 1].fromBlock <= blockNumber) {
+      return checkpoints[account][nCheckpoints - 1].votes;
+    }
+    // Next check implicit zero balance
+    if (checkpoints[account][0].fromBlock > blockNumber) {
+      return 0;
+    }
+    uint256 lower = 0;
+    uint256 upper = nCheckpoints - 1;
+    while (upper > lower) {
+      uint256 center = upper - (upper - lower) / 2; // ceil, avoiding overflow
+      Checkpoint memory cp = checkpoints[account][center];
+      if (cp.fromBlock == blockNumber) {
+        return cp.votes;
+      } else if (cp.fromBlock < blockNumber) {
+        lower = center;
+      } else {
+        upper = center - 1;
+      }
+    }
+    return checkpoints[account][lower].votes;
+  }
 
   /*´:°•.°+.*•´.*:˚.°*.˚•´.°:°•.°•.*•´.*:˚.°*.˚•´.°:°•.°+.*•´.*:*/
   /*                    EXTERNAL FUNCTIONS                      */
@@ -109,21 +165,44 @@ contract govLOCKS is ERC20 {
 
   function _moveDelegates(address srcRep, address dstRep, uint256 amt) internal {
     if (srcRep != dstRep && amt > 0) {
-    //   if (srcRep != address(0)) {
-    //     uint32 srcRepNum = numCheckpoints[srcRep];
-    //     uint96 srcRepOld = srcRepNum > 0 ? checkpoints[srcRep][srcRepNum - 1].votes : 0;
-    //     uint96 srcRepNew = sub96(srcRepOld, amt, "Uni::_moveVotes: vote amount underflows");
-    //     _writeCheckpoint(srcRep, srcRepNum, srcRepOld, srcRepNew);
-    //   }
-    // if (dstRep != address(0)) {
-    //   uint32 dstRepNum = numCheckpoints[dstRep];
-    //   uint96 dstRepOld = dstRepNum > 0 ? checkpoints[dstRep][dstRepNum - 1].votes : 0;
-    //   uint96 dstRepNew = add96(dstRepOld, amt, "Uni::_moveVotes: vote amount overflows");
-    //   _writeCheckpoint(dstRep, dstRepNum, dstRepOld, dstRepNew);
-    // }
-
+      if (srcRep != address(0)) {
+        uint256 srcRepNum = numCheckpoints[srcRep];
+        uint256 srcRepOld = srcRepNum > 0 ? checkpoints[srcRep][srcRepNum - 1].votes : 0;
+        uint256 srcRepNew = srcRepOld - amt;
+        _writeCheckpoint(srcRep, srcRepNum, srcRepOld, srcRepNew);
+      }
+      if (dstRep != address(0)) {
+        uint256 dstRepNum = numCheckpoints[dstRep];
+        uint256 dstRepOld = dstRepNum > 0 ? checkpoints[dstRep][dstRepNum - 1].votes : 0;
+        uint256 dstRepNew = dstRepOld + amt;
+        _writeCheckpoint(dstRep, dstRepNum, dstRepOld, dstRepNew);
+      }
     }
   }
 
+  function _writeCheckpoint(
+    address delegatee, 
+    uint256 nCheckpoints, 
+    uint256 oldVotes, 
+    uint256 newVotes
+  ) internal {
+    if (nCheckpoints > 0 && checkpoints[delegatee][nCheckpoints - 1].fromBlock == block.number) {
+      checkpoints[delegatee][nCheckpoints - 1].votes = newVotes;
+    } else {
+      checkpoints[delegatee][nCheckpoints] = Checkpoint(block.number, newVotes);
+      numCheckpoints[delegatee] = nCheckpoints + 1;
+    }
+    emit DelegateVotesChanged(delegatee, oldVotes, newVotes);
+  }
+
+
+  /*´:°•.°+.*•´.*:˚.°*.˚•´.°:°•.°•.*•´.*:˚.°*.˚•´.°:°•.°+.*•´.*:*/
+  /*                   IMPLEMENTATION FUNCTION                  */
+  /*.•°:°.´+˚.*°.˚:*.´•*.+°.•°:´*.´•*.•°.•°:°.´:•˚°.*°.˚:*.´+°.•*/
+
+
+  function _afterTokenTransfer(address from, address to, uint256 amt) internal override {
+    _moveDelegates(delegates[from], delegates[to], amt);
+  }
 
 }
