1687b80f7fca4792b69312985d31cb537bc1d6fd
0xgeeb <0xgeeb@gmail.com>
2023-12-14 19:19:55 -0500

    added product page

--- frontend/components/home/products/index.tsx
+++ frontend/components/home/products/index.tsx
@@ -6,6 +6,48 @@ export const Products = () => {
         src="/real.png"
         alt="real"
       />
+      <h1 className="absolute top-[15%] left-[43%] font-acme text-[3.5rem]">Goldilocks AMM (GAMM)</h1>
+      <div className="absolute h-[16%] w-[42%] top-[24%] left-[43.5%] font-acme text-[1.5rem] flex flex-row justify-between">
+        <div className="h-[100%] w-[30%] border-2 border-slate-700 rounded-md bg-white bg-grid-slate-200">
+          <div className="h-[100%] flex flex-col justify-center items-center text-center rounded-md">
+            <p>utilizes two</p>
+            <p>liquidity pools</p>
+          </div>
+        </div>
+        <div className="h-[100%] w-[30%] border-2 border-slate-700 rounded-md bg-white bg-grid-slate-200">
+          <div className="h-[100%] flex flex-col justify-center items-center text-center rounded-md">
+            <p>liquidity comprised</p>
+            <p>of $HONEY</p>
+          </div>
+        </div>
+        <div className="h-[100%] w-[30%] border-2 border-slate-700 rounded-md bg-white bg-grid-slate-200">
+          <div className="h-[100%] flex flex-col justify-center items-center text-center rounded-md">
+            <p>trade between</p>
+            <p>$LOCKS & $HONEY</p>
+          </div>
+        </div>
+      </div>
+      <h1 className="absolute top-[56%] left-[43%] font-acme text-[3.5rem]">Goldilend</h1>
+      <div className="absolute h-[16%] w-[42%] top-[65%] left-[43.5%] font-acme text-[1.5rem] flex flex-row justify-between">
+        <div className="h-[100%] w-[30%] border-2 border-slate-700 rounded-md bg-white bg-grid-slate-200">
+          <div className="h-[100%] flex flex-col justify-center items-center text-center rounded-md">
+            <p>bong bera & rebase</p>
+            <p>nft lending</p>
+          </div>
+        </div>
+        <div className="h-[100%] w-[30%] border-2 border-slate-700 rounded-md bg-white bg-grid-slate-200">
+          <div className="h-[100%] flex flex-col justify-center items-center text-center rounded-md">
+            <p>LP $BERA</p>
+            <p>to receive $gBERA</p>
+          </div>
+        </div>
+        <div className="h-[100%] w-[30%] border-2 border-slate-700 rounded-md bg-white bg-grid-slate-200">
+          <div className="h-[100%] flex flex-col justify-center items-center text-center rounded-md">
+            <p>idle $BERA liquidity</p>
+            <p>staked in Berachain</p>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
