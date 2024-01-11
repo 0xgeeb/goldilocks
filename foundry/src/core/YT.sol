88e7d950526805c226244a79666be99abe4cc45d
0xgeeb <0xgeeb@gmail.com>
2024-01-10 21:28:45 -0500

    worked on ys contract

---
+++ foundry/src/core/YT.sol
@@ -0,0 +1,47 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+
+// |============================================================================================|
+// |    ______      _____    __      _____    __   __       _____     _____   __  __   ______   |
+// |   /_/\___\    ) ___ (  /\_\    /\ __/\  /\_\ /\_\     ) ___ (   /\ __/\ /\_\\  /\/ ____/\  |
+// |   ) ) ___/   / /\_/\ \( ( (    ) )  \ \ \/_/( ( (    / /\_/\ \  ) )__\/( ( (/ / /) ) __\/  |
+// |  /_/ /  ___ / /_/ (_\ \\ \_\  / / /\ \ \ /\_\\ \_\  / /_/ (_\ \/ / /    \ \_ / /  \ \ \    |
+// |  \ \ \_/\__\\ \ )_/ / // / /__\ \ \/ / // / // / /__\ \ )_/ / /\ \ \_   / /  \ \  _\ \ \   |
+// |   )_)  \/ _/ \ \/_\/ /( (_____() )__/ /( (_(( (_____(\ \/_\/ /  ) )__/\( (_(\ \ \)____) )  |
+// |   \_\____/    )_____(  \/_____/\/___\/  \/_/ \/_____/ )_____(   \/___\/ \/_//__\/\____\/   |
+// |                                                                                            |
+// |============================================================================================|
+// ==============================================================================================
+// ======================================== Yield Token =========================================
+// ==============================================================================================
+
+
+import { ERC20 } from "../../lib/solady/src/tokens/ERC20.sol";
+
+
+/// @title Yield Token
+/// @notice Token representing yield
+/// @author geeb
+/// @author ampnoob
+contract YT is ERC20 {
+
+  /// @notice Returns the name of the $LOCKS token
+  function name() public pure override returns (string memory) {
+    return "Yield Token";
+  }
+
+  /// @notice Returns the symbol of the $LOCKS token
+  function symbol() public pure override returns (string memory) {
+    return "YT";
+  }
+
+  function mint(address _to, uint256 _amount) external {
+    _mint(_to, _amount);
+  }
+
+  function burn(address _to, uint256 _amount) external {
+    _burn(_to, _amount);
+  }
+
+}
