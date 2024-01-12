46b5228924625577deaf73b72e75be18bacea489
0xgeeb <0xgeeb@gmail.com>
2024-01-11 20:40:49 -0500

    added functions to ys

--- foundry/src/core/YieldSplitter.sol
+++ foundry/src/core/YieldSplitter.sol
@@ -19,7 +19,7 @@ pragma solidity ^0.8.19;
 
 import { FixedPointMathLib } from "../../lib/solady/src/utils/FixedPointMathLib.sol";
 import { SafeTransferLib } from "../../lib/solady/src/utils/SafeTransferLib.sol";
-// import { IERC20 } from "../../lib/solady/src/tokens/IERC20.sol";
+import { ERC20 } from "../../lib/solady/src/tokens/ERC20.sol";
 import { OT } from "./OT.sol";
 import { YT } from "./YT.sol";
 
@@ -32,8 +32,8 @@ contract YieldSplitter {
 
   OT ot;
   YT yt;
-  uint256 startDate;
-  uint256 endDate;
+  uint256 startTime;
+  uint256 endTime;
   uint256 concludeTime;
   uint256 finalYield;
   address treasury;
@@ -43,7 +43,9 @@ contract YieldSplitter {
 
   error InsufficientTime();
   error NotExpired();
+  error NotConcluded();
   error AlreadyConcluded();
+  error ExcessiveRedeem();
 
   constructor(
     address _ot, 
@@ -62,7 +64,7 @@ contract YieldSplitter {
   /// @notice Deposits $HONEY into vault to receive ownership and yield tokens
   /// @param amount Amount of tokens to deposit
   function deposit(uint256 amount) external {
-    uint256 remainingTime = endDate - block.timestamp;
+    uint256 remainingTime = endTime - block.timestamp;
     if(remainingTime < 30 days) revert InsufficientTime();
     uint256 timeshare = remainingTime / 365 days;
     _claim();
@@ -71,15 +73,55 @@ contract YieldSplitter {
     yt.mint(msg.sender, amount * timeshare);
   }
 
+  /// @notice Concludes the vault at expiry
   function conclude() external {
-    if(block.timestamp < endDate) revert NotExpired();
+    if(block.timestamp < endTime) revert NotExpired();
     if(concluded) revert AlreadyConcluded();
     concluded = true;
     concludeTime = block.timestamp;
-    // uint256 finalYield = (IERC20(ibgt).balanceOf(address(this)) / 100) * 98;
-    // IERC20(ibgt).transfer(treasury, IERC20(ibgt).balanceOf(address(this)) / 100) * 2;
+    uint256 finalYield = (ERC20(ibgt).balanceOf(address(this)) / 100) * 98;
+    SafeTransferLib.safeTransfer(ibgt, treasury, (ERC20(ibgt).balanceOf(address(this)) / 100) * 2);
     //code to unstake all honey from the vault (and, if not done automatically, claim outstanding yield and convert it to IBGT)
     //code to unstake all the contract's IBGT (and send any outstanding IBGT staking rewards to treasury)
+  }
+
+  /// @notice Redeems yield tokens for share of yield accrued to vault
+  /// @param amount Amount of tokens to redeem
+  function redeemYield(uint256 amount) external {
+    if(block.timestamp < concludeTime + 36 hours || !concluded) revert NotConcluded();
+    uint256 yieldShare = FixedPointMathLib.mulWad(amount, ERC20(yt).totalSupply());
+    //todo: ask
+    uint256 rawYield = 0;
+    uint256 claimable = FixedPointMathLib.divWad(rawYield, yieldShare);
+    YT(yt).burn(msg.sender, amount);
+
+    SafeTransferLib.safeTransferFrom(ibgt, address(this), msg.sender, claimable);
+  }
+
+  /// @notice Withdraws assets from the vault 
+  /// @param amount Amount of tokens to redeem
+  function redeemOwnership(uint256 amount) external {
+    uint256 remainingTime = block.timestamp > endTime ? 0 : endTime - block.timestamp;
+    uint256 totalTimeDurationRatio = FixedPointMathLib.divWad(remainingTime, endTime - startTime);
+    if(ERC20(yt).balanceOf(msg.sender) < FixedPointMathLib.mulWad(amount, totalTimeDurationRatio)) revert ExcessiveRedeem();
+    OT(ot).burn(msg.sender, amount);
+    YT(yt).burn(msg.sender, FixedPointMathLib.divWad(amount, totalTimeDurationRatio));
+    if(remainingTime == 0) {
+      SafeTransferLib.safeTransferFrom(honey, address(this), msg.sender, (amount / 100) * 98);
+      SafeTransferLib.safeTransferFrom(honey, address(this), treasury, (amount / 100) * 2);
+    }
+    else {
+      SafeTransferLib.safeTransferFrom(honey, address(this), msg.sender, amount);
+    }
+  }
+
+  /// @notice Renews a concluded vault after users have chance to claim
+  //todo: ask about amount
+  function renew() external {
+    if(block.timestamp < concludeTime + 2 weeks || !concluded) revert NotConcluded();
+    startTime = block.timestamp;
+    endTime = startTime + 365 days;
+    concluded = false;
   }
 
   function _claim() internal {
